d5516a12046271038546e604d4ea26b503097421
Joydeep Paul <joydeeprnp8821@gmail.com>
2025-09-17 14:52:01 +0530

    Dashboard updated

--- frontend/src/pages/DashBoard.jsx
+++ frontend/src/pages/DashBoard.jsx
@@ -1,7 +1,9 @@
 import React from 'react'
 
 const DashBoard = () => {
-  return <div className="text-5xl text-center mt-25 "> This is DashBoard</div>;
+  return (
+    <div className='text-5xl text-center mt-25 '> This is DashBoard</div>
+  )
 }
 
 export default DashBoard
