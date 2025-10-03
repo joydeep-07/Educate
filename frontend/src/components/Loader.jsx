c652a11e072ed68c89e469db29264aa804e47383
Joydeep Paul <joydeeprnp8821@gmail.com>
2025-10-03 22:37:23 +0530

    Loader Designed

--- frontend/src/components/Loader.jsx
+++ frontend/src/components/Loader.jsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import React from "react";
+import Lottie from "lottie-react";
+import loading from "../assets/animation/Loading.json";
 
 const Loader = () => {
   return (
-    <div className='text-5xl text-center mt-25 animate-pulse text-gray-700 font-medium' >Loader</div>
-  )
-}
+    <div className="flex flex-col items-center justify-center min-h-screen">
+      <div className="w-48 h-48">
+        <Lottie animationData={loading} loop autoplay />
+      </div>
+      <p className="mt-6 text-gray-600 font-medium text-lg animate-pulse">
+        Loading, please wait...
+      </p>
+    </div>
+  );
+};
 
-export default Loader
+export default Loader;
