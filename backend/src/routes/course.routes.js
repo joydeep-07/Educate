7211a63e67c7fa53c1926993fe810839275a9f9f
Joydeep Paul <joydeeprnp8821@gmail.com>
2025-10-05 23:31:57 +0530

    Course Route Updated

--- backend/src/routes/course.routes.js
+++ backend/src/routes/course.routes.js
@@ -9,19 +9,11 @@ import {
 
 const router = express.Router();
 
-// POST: Add course
+
 router.post("/add", addCourse);
-
-// GET: Get all courses
 router.get("/", getCourses);
-
-// GET: Get course by ID
 router.get("/:id", getCourseById);
-
-// PUT: Update course by ID
 router.put("/:id", updateCourse);
-
-// DELETE: Delete course by ID
 router.delete("/:id", deleteCourse);
 
 export default router;
