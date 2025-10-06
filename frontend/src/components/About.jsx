2473ae22987c3b969cbc776f25d4b6f329512d9d
Joydeep Paul <joydeeprnp8821@gmail.com>
2025-10-06 19:47:49 +0530

    Banner Created

--- frontend/src/components/About.jsx
+++ frontend/src/components/About.jsx
@@ -2,10 +2,19 @@ import React from 'react'
 
 const About = () => {
   return (
-   <>
-   
-   </>
-  )
+    <>
+      <div className='min-h-screen'>
+        <div className="banner h-70 w-[100vw] bg-sky-800/20 flex justify-center items-center">
+          <div className='w-80 '>
+            <div className="text-5xl font-bold text-white uppercase text-left">
+              <h1>Think </h1>
+              <h1>Different</h1>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default About
