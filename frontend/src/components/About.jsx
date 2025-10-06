69d34ec67177b68d8e1067ca3b2054eab3c52f04
Joydeep Paul <joydeeprnp8821@gmail.com>
2025-10-06 19:37:14 +0530

    Creating About page

--- frontend/src/components/About.jsx
+++ frontend/src/components/About.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div className='text-center text-gray-700 text-md font-medium tracking-wide uppercase'>About</div>
+   <>
+   
+   </>
   )
 }
 
