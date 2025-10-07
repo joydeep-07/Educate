4f952e16aa81faacd1c514e63d46f6c508b0d219
Joydeep Paul <joydeeprnp8821@gmail.com>
2025-10-07 23:58:22 +0530

    Updated

--- frontend/src/pages/Profile.jsx
+++ frontend/src/pages/Profile.jsx
@@ -4,7 +4,7 @@ import EnrolledCourses from '../components/EnrolledCourses'
 const Profile = () => {
   return (
     <div>
-      <h1 className='text-center mt-25 uppercase text-3xl pb-10 text-gray-700'>this is Profile </h1>
+      <h1 className='text-center mt-25 uppercase text-3xl pb-10 text-gray-700'>Profile </h1>
       <EnrolledCourses />
     </div>
   );
